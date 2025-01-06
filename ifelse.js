b60a8b55856fe1435031b7b39adb2eb794fec4df
Ville V. Vanninen <ville@pumpula.net>
2025-01-06 12:58:11 +0200

    Todos, clean up

--- ifelse.js
+++ ifelse.js
@@ -1,7 +1,14 @@
+/*TODO:
+- This needs a way to do or and not conditions on the if elements. Ands can be done via two nested ifs. Ors could be done sibling if elements, but that would mean duplicating content. Else works as not, but again with sibling or elements would need duplicate content.
+Fun idea: <if- number="10">Value is 10</if-> Like what if the name, value pair is just attribute name and its value. So number="10" would match input <name="number" value="10">. This would introduce either reserved attribute names or
+- This is crazy, linters will yell at you, but again fun! <if- for="number" not "value1" "value2"></if->
+*/
+
+
 export class If extends HTMLElement {
 
   //Not really needed, but a nice reminder of all the attributes this element officially accepts.
-  static observedAttributes = ['form', 'for', 'value']
+  static observedAttributes = ['form', 'for', 'value', 'not']
 
   get form() {
     return document.forms[this.getAttribute('form')]
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ customElements.define("else-", Else);
 const form = document.forms['giftifier']
 form.addEventListener('input', (e) => derive(e.target.form.elements))
 
-//TODO: move device into ifelse or something so they can orchestrate running order correctly. So all derivations get run first and only after that the if else thingies get run.
+//TODO: move derive into ifelse or something so they can orchestrate running order correctly. So all derivations get run first and only after that the if else thingies get run.
 function derive($) {
   // console.log($.check)
   $.combined.value = $.first.value + $.second.value
