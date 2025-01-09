b25a65fe89cd6f49c343223f972e71cabfbf3c60
Ville V. Vanninen <ville@pumpula.net>
2025-01-09 02:05:13 +0200

    Proper event listening and waiting for domready
    When connected, initial evaluation happens only when dom is ready.
    
    Configurable debounce.

--- ifelse.js
+++ ifelse.js
@@ -9,6 +9,7 @@ Fun idea: <if- number="10">Value is 10</if-> Like what if the name, value pair i
 export class If extends HTMLElement {
   //TODO: `name` attribute could refer to formData name and `for´ attribute could refer to a specific input element, just like <label for=""> does? That then would need a check so the code would use formData.get() with `for` attribute and getAll() with `name` attribute
   static observedAttributes = ['form', 'name', 'value', 'not']
+  static debounceDelay = 33.333 //Maybe use throttle for ifs instead of debounce? Max 30fps ok? This isn't heavy at all and even less so if the form property was cached. I don't want this to be synchronous because I might want to modify some hidden inputs in response to an input event and have this if react to those hidden inputs too.
 
   get form() {
     return document.forms[this.getAttribute('form')]
@@ -20,16 +21,54 @@ export class If extends HTMLElement {
 
   constructor() {
     super()
-
-    // Let's make this async, so any value manipulations on the form and stuff have been run before this gets evaluated.
-    // TODO: probably use requestAnimationFrame instead and let browser decide the debounce threshold.
-    this.evaluateDebounced = debounce(this.evaluate.bind(this), 16)
-    this.form && this.form.addEventListener('input', this.evaluateDebounced)
-    this.evaluateDebounced()
+    this.evaluate = debounce(this.evaluate.bind(this), If.debounceDelay)
+    this.handleEvent = this.handleEvent.bind(this)
   }
 
-  evaluateDebounced
+  handleEvent(e) {
+    if (e.target.form === this.form) this.evaluate()
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    this.evaluate()
+  }
+
+  connectedCallback() {
+    if (document.readyState === "loading") {
+      document.addEventListener("DOMContentLoaded",
+        this.connectedCallback.bind(this),
+        {once: true}
+      )
+      return
+    }
+    this.listen()
+    this.evaluate()
+  }
+  disconnectedCallback() {
+    this.unlisten()
+  }
+  adoptedCallback() {
+    this.relisten()
+  }
+
+  #listening
+  listen() {
+    !this.#listening && document.addEventListener('input', this.handleEvent)
+    !this.#listening && document.addEventListener('change', this.handleEvent)
+    this.#listening = true
+  }
+  unlisten() {
+    document.removeEventListener('input', this.handleEvent)
+    document.removeEventListener('change', this.handleEvent)
+    this.#listening = false
+  }
+  relisten() {
+    this.unlisten()
+    this.listen()
+  }
+
   evaluate() {
+    if (!this.form) return
     const data = new FormData(this.form)
     // Using formData semantics for value check, so works fine with multiple inputs of the same name, like is often the case with lists of checkboxes, but using this requires that you understand how forms work.
     let isTrue = data.getAll(this.name).includes(this.value)
